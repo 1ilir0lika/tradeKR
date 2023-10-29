394718fb3328d6d9f8db0b96ddb933ef3e1509e3
ilir lika <49962713+1ilir0lika@users.noreply.github.com>
2023-10-29 15:17:23 +0100

    Update main.py
    fix sticker animati che bloccavano tutto

--- main.py
+++ main.py
@@ -18,25 +18,33 @@ headers = ["index","best price","delta","margine"]
 link='https://krunker.io/social.html?p=market&i='
 all_rows=[]
 min_margine=100
+#SE VOGLIO FILTRARE LE SKIN CON AL MASSIMO IL MIO SALDO FACCIO COSÌ,ALTRIMENTI LO DICHIARO A MANO
+#mykr=driver.find_element(By.ID,'profileKR').text
+#mykr=format(mykr)
+#mykr=int(mykr)
 mykr=2500
-for i in range(868,8000):
+#tempo da aspettare prima di cambiare pagina dicendo che nontrova l'elemento
+driver.implicitly_wait(2)
+for i in range(0,8000):
     print("index : "+str(i))
     row=[]
     driver.get(link+str(i))
     #time.sleep(1)
     #prices=driver.find_element(By.XPATH,'/html/body/div[2]/div[15]/div[2]/div[1]/div[2]')
-    #SE VOGLIO FILTRARE LE SKIN CON AL MASSIMO IL MIO SALDO FACCIO COSÌ,ALTRIMENTI LO DICHIARO A MANO
-    #mykr=driver.find_element(By.ID,'profileKR').text
-    #mykr=format(mykr)
-    #mykr=int(mykr)
     try:
         best=driver.find_element(By.CLASS_NAME,'marketCard').text
-        best=format(best)
-        best_price=int(best.split('\n')[2])
+        if (best != ''):
+            best=format(best)
+            best_price=int(best.split('\n')[2])
+        else:
+            best_price=0
     except NoSuchElementException:
         best_price=0
     try:
         second=driver.find_element(By.XPATH,'/html/body/div[2]/div[15]/div[2]/div[2]/div[2]').text
+        if (second == ''):
+            #se non faccio cosí con gli sticker animati si rompe tutto
+            second=driver.find_element(By.XPATH,'/html/body/div[2]/div[15]/div[2]/div[2]/div[3]').text
         second=format(second)
         second_price=int(second)
     except NoSuchElementException:
