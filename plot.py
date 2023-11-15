70deea1c60bdf4b92972d56e3c684101c3e24447
ilir lika <49962713+1ilir0lika@users.noreply.github.com>
2023-11-15 22:37:00 +0100

    Update plot.py
    sempre da verificare,comunque apre cliccando sopra pallino la sua pagina web

--- plot.py
+++ plot.py
@@ -1,18 +1,20 @@
 import datetime
 import matplotlib.pyplot as plt
 import mplcursors
+import webbrowser
 import sys
 
 date = datetime.date.today()
 index = []
 prices = []
 ratio = []
+
 nome_file = 'data/' + str(date) + '.pr'
 
 try:
     with open(nome_file, 'r') as start_file:
         list_of_lines = start_file.readlines()
-        all_rows = eval(list_of_lines[0].strip())
+        all_rows = eval(list_of_lines[0].strip())[:20]  # Prendi solo i primi 20 elementi
         for r in all_rows:
             index.append(r[0])
             prices.append(r[1])
@@ -26,6 +28,9 @@ except Exception as e:
     print(f"An error occurred: {e}")
     sys.exit()
 
+# URL base
+base_url = 'https://krunker.io/social.html?p=market&i='
+
 # Creazione del plot 2D con trasparenza
 scatter = plt.scatter(prices, ratio, alpha=0.5)
 
@@ -35,6 +40,16 @@ plt.ylabel('Ratio')
 
 # Regola il layout del grafico per evitare sovrapposizioni di etichette
 plt.tight_layout()
+
+def on_click(event):
+    contains, info = scatter.contains(event)
+    if contains:
+        ind = info['ind'][0]
+        url = base_url + str(index[ind])
+        webbrowser.open(url)
+
+# Collega la funzione on_click all'evento di pressione del pulsante
+plt.gcf().canvas.mpl_connect('button_press_event', on_click)
 
 # Aggiungi le etichette con i valori di index quando si passa sopra ai punti
 cursor = mplcursors.cursor(hover=True)
